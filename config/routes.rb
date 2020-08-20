2e5154d73a10b6a018a646df78695568ec7c9ffc
shibatasuguru0311 <s.shibata.stuvwx@gmail.com>
2020-08-20 12:16:32 +0900

    model-assosiation

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'works#index'
+resources :users, only: [:show,:edit,:update]
+resources :messages, only: [:create]
+resources :rooms, only: [:create,:show]
 end
