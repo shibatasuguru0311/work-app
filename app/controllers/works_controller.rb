c12ef083fe3f725961e68f6b7e4ea59b7be07632
shibatasuguru0311 <s.shibata.stuvwx@gmail.com>
2020-08-25 06:38:48 +0900

    ログイン後の実装

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,4 +1,31 @@
 class WorksController < ApplicationController
   def index
   end
+
+  def new
+    if user_signed_in?
+      @work = Work.new
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save!
+    else
+      render :new
+    end
+  end
+
+  def show
+    @work = Work.find(params[:id])
+  end
+
+  private
+  def work_params
+    params.require(:work).permit(:name, :introduction, :address, :url, :photo, user_ids: [])
+  end
+
 end
