561437a787e4bb2731ccf4f5af752109f3b1881e
shibatasuguru0311 <s.shibata.stuvwx@gmail.com>
2020-08-28 00:00:40 +0900

    show-edit

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -22,6 +22,30 @@ class WorksController < ApplicationController
 
   def show
     @work = Work.find(params[:id])
+    @works = Work.order('created_at DESC')
+  end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def destroy
+    @work = Work.find(params[:id])
+      if @work.destroy
+        @works = Work.order('created_at DESC')
+        render :index
+      else
+        render :show
+      end
+  end
+
+  def update
+    @work = Work.find(params[:id])
+      if @work.update(work_params)
+        render :show
+      else
+        render :edit
+      end
   end
 
   private
